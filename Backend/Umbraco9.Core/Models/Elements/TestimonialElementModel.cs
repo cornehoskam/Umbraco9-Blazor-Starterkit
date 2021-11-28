1071c791dc66855a5f83dc618f131267fd0d50d6
corne.hoskam <choskam@hotmail.com>
2021-11-28 11:18:28 +0100

    Start on Testimonial Component & Umbraco Collection

---
+++ Backend/Umbraco9.Core/Models/Elements/TestimonialElementModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco9.Core.Models.Miscellaneous;
+using Umbraco9.Core.UmbracoModels;
+
+namespace Umbraco9.Core.Models.Elements
+{
+    [Serializable]
+    public class TestimonialElementModel : IElementModel
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public List<TestimonialItemModel> Testimonials {get; set;}
+        public string UmbracoContentType => TestimonialElement.ModelTypeAlias;
+
+        public TestimonialElementModel() { }
+
+        public TestimonialElementModel(TestimonialElement testimonialElement) 
+        {
+            Title = testimonialElement.Title;
+            Subtitle = testimonialElement.Subtitle;
+            if(testimonialElement.Testimonials != null && testimonialElement.Testimonials.Any())
+               Testimonials = testimonialElement.Testimonials.Select(x => new TestimonialItemModel(x as TestimonialItem)).ToList();
+            
+        }
+    }
+}
